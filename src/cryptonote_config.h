57aa0c760b2c4ca4c43d12eaafdcae97c299bf01
CryptoKarma <sverigesstatsminister@hotmail.com>
2014-10-12 20:51:57 +0200

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -34,7 +34,7 @@
 //TODO Define number of smallest units in one coin
 #define COIN                                            
 //TODO Define default fee for transactions
-#define DEFAULT_FEE                                     
+#define DEFAULT_FEE                                     10000
 
 //TODO Define preferred block's target time
 #define DIFFICULTY_TARGET                               360 // seconds
